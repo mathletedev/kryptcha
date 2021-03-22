13f83c850d7919e836eddd6dccf5140d77e4d0e8
MathleteDev <mathletedev@gmail.com>
2021-03-21 17:20:23 -0700

    CRUD mutations

--- server/src/resolvers/ProductResolver.ts
+++ server/src/resolvers/ProductResolver.ts
@@ -1,4 +1,4 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Query, Resolver } from "type-graphql";
 import { Product } from "../entities/Product";
 
 @Resolver()
@@ -6,5 +6,55 @@ export class ProductResolver {
 	@Query(() => [Product])
 	async products() {
 		return await Product.find();
+	}
+
+	@Mutation(() => Product!)
+	async createProduct(
+		@Arg("name") name: string,
+		@Arg("description") description: string,
+		@Arg("price") price: number,
+		@Arg("stock") stock: number
+	) {
+		const product = Product.create({
+			name,
+			description,
+			price,
+			stock
+		});
+		return await product.save();
+	}
+
+	@Query(() => Product!, { nullable: true })
+	async findProduct(@Arg("id") id: string) {
+		return await Product.findOne(id);
+	}
+
+	@Mutation(() => Product!, { nullable: true })
+	async deleteProduct(@Arg("id") id: string) {
+		const product = await Product.findOne(id);
+		if (product) {
+			await Product.delete(id);
+			return product;
+		}
+		return null;
+	}
+
+	@Mutation(() => Product!, { nullable: true })
+	async updateProduct(
+		@Arg("id") id: string,
+		@Arg("name") name: string,
+		@Arg("description") description: string,
+		@Arg("price") price: number,
+		@Arg("stock") stock: number
+	) {
+		let product = await Product.findOne(id);
+		if (product) {
+			product.name = name;
+			product.description = description;
+			product.price = price;
+			product.stock = stock;
+			return await product.save();
+		}
+		return null;
 	}
 }
